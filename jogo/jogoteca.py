738ccb82a81cb1180d0c32f16dd9cf17734501a4
EvErToN <evertonflorenciodasilva@outlook.com>
2024-02-23 19:03:10 -0300

    separando o codigo em html do logico em python

--- jogo/jogoteca.py
+++ jogo/jogoteca.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    return 'index.html'
+    return render_template('index.html')
 
 app.run()
