3b754cf092902357c7b79236ba60693a3422ca40
EvErToN <evertonflorenciodasilva@outlook.com>
2024-02-24 01:39:24 -0300

    criei duas novas rotas o /criar e o /novo, o novo vai carregar o novo arquivo html que criei e o criar ira pegar o nome,categoria e o console da pagina e adicionar em uma na lista

--- jogo/jogoteca.py
+++ jogo/jogoteca.py
@@ -1,16 +1,30 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
-class jogo :
+class Jogo :
     def __init__(self, nome, categoria, console):
         self.nome = nome
         self.categoria = categoria
         self.console = console
+jogos1 = Jogo('domm','fps','xbox')
+jogos2 = Jogo('halo','fps','xbox')
+lista = [jogos1, jogos2]
 @app.route('/inicio')
-def ola():
-    jogos1 = jogo('domm','fps','xbox')
-    jogos2 = jogo('halo','fps','xbox')
-    lista = [jogos1, jogos2 ]
-    return render_template('index.html', titulo = 'jogos', jogos = lista)
+def apresenta():
+
+    return render_template('index.html', titulo='jogos', jogos=lista)
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html',titulo='novo jogo')
+
+@app.route('/criar')
+def jogo():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('novo.html',titulo='jogos',jogos=lista)
 
 app.run()
