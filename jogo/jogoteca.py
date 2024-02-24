91e6d10a0ffece34bd81f19a3db4f0ef7c2d3801
EvErToN <evertonflorenciodasilva@outlook.com>
2024-02-24 00:36:19 -0300

    criei um construtor que recebe o nome, categoria e o console do game , guardando no jogo1 e 2 e armazenando na lista, assim tendo acesso no codigo index.html

--- jogo/jogoteca.py
+++ jogo/jogoteca.py
@@ -1,10 +1,16 @@
 from flask import Flask,render_template
 
 app = Flask(__name__)
-
+class jogo :
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
 @app.route('/inicio')
 def ola():
-    lista = ['good of war', 'the last of us', 'horizon']
+    jogos1 = jogo('domm','fps','xbox')
+    jogos2 = jogo('halo','fps','xbox')
+    lista = [jogos1, jogos2 ]
     return render_template('index.html', titulo = 'jogos', jogos = lista)
 
 app.run()
