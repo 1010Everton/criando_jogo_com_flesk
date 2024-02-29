265f1c14eb3e015b87b4635474e2b2599d2fcd4a
EvErToN <evertonflorenciodasilva@outlook.com>
2024-02-29 11:31:26 -0300

    login terminado, criei uma aba de login aonde o usuario tem que colocar uma senha e apos acertar a senha aparecera uma mensagem e ele sera redirecionado de acordo com sua senha se esteja errada ou não

--- jogo/jogoteca.py
+++ jogo/jogoteca.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 app = Flask(__name__)
+app.secret_key = 'alura'
 class Jogo :
     def __init__(self, nome, categoria, console):
         self.nome = nome
@@ -9,12 +10,12 @@ class Jogo :
 jogos1 = Jogo('domm','fps','xbox')
 jogos2 = Jogo('halo','fps','xbox')
 lista = [jogos1, jogos2]
-@app.route('/inicio')
+@app.route('/')
 def index():
 
     return render_template('lista.html', titulo='jogos', jogos=lista)
 
-@app.route('/')
+@app.route('/novo')
 def novo():
     return render_template('novo.html',titulo='novo jogo')
 
@@ -28,14 +29,14 @@ def criar():
     return redirect('/inicio')
 @app.route('/login')
 def login():
-    return ('login.html')
-@app.route('/autentica')
+    return render_template('login.html')
+@app.route('/autenticar', methods=['POST',])
 def autentica():
-    if '123' == request.form ['senha']:
-        return ('/')
+    if '123' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado'] + 'logado com sucesso!')
+        return redirect('/novo')
     else:
-        return ('/login')
-
-
-
+        flash('usuario não logado')
+        return redirect('/login')
 app.run(debug=True)
